f4d8b332d1ddaa16acc38f29d8609249669ea118
MrCornBallJB <jensblixt@hotmail.com>
2024-11-29 08:46:59 +0100

    Update Program.cs
    Lade till så man kan välja om man vill ha m/s eller km/h

--- WindchillCalc/Program.cs
+++ WindchillCalc/Program.cs
@@ -4,13 +4,13 @@
     {
         static void Main(string[] args)
         {
-            bool fortsätt = true;
+            
 
             while (true)
 
             {
 
-                Console.WriteLine(" 1: Beräkna windchill-faktor: ");
+                Console.WriteLine("1: Beräkna windchill-faktor: ");
                 Console.WriteLine("2: Avsluta: ");
                 int userinput = int.Parse(Console.ReadLine());   
 
@@ -27,6 +27,11 @@
                         Environment.Exit(0);
                         break;
                 }
+                if (userinput != 1 && userinput != 2)
+                {
+                    Console.WriteLine("Fel input!");
+                }
+                Console.WriteLine();
 
 
 
@@ -56,24 +61,31 @@
             Console.WriteLine("Ange utomhustempraturen i celsius: ");
             Temp = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Fyll i vindhastighetet ( ange i m/s eller km/h ): ");
+            Console.WriteLine("1: m/s");
+            Console.WriteLine("2: km/h");
+
+            int enhet = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Vindhastigheten");
             Wind = Convert.ToDouble(Console.ReadLine());
-            
-
-            if (Wind < 4.9 || Wind > 4.9)
-            {
-
-
-                Console.WriteLine("Vind hastighet får inte vara under 4.9");
-
-
-
-
-            }
-            else
+            if (enhet == 1)
             {
                 WindChillcalc = 13.12 + 0.6215 * Temp - 11.37 * Math.Pow(Wind, 016) + 0.3965 * Temp * Math.Pow(Wind, 0.16);
                 Console.WriteLine("Känns som " + WindChillcalc.ToString() + " grader celsius");
             }
+            else if (enhet == 2)
+            {
+                WindChillcalc = 13.12 + 0.6215 * Temp - 11.37 * Math.Pow(Wind, 016) + 0.3965 * Temp * Math.Pow(Wind, 0.16);
+                Console.WriteLine("Känns som " + WindChillcalc.ToString() + " grader celsius");
+            }
+            else
+            {
+                Console.WriteLine("Fel input!");
+            }
+            
+            
+
+            
 
 
 
